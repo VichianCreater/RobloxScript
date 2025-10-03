0ee16679ff2a35c6bc25c338c7a2d6aeb303bc14
Vichian <68704647+VichianCreater@users.noreply.github.com>
2025-10-03 12:23:42 +0700

    Add files via upload

---
+++ DA_Script.lua
@@ -0,0 +1,65 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Dragon Adventure | Test ",
+    SubTitle = ".by Vichian",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(480, 360),
+    Acrylic = true,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Fluent.Options
+
+do
+    local EggCollectToggle = Tabs.Main:AddToggle("EggCollect", {Title = "AUTO - Collect(EGG)", Default = false })
+    local isCollectingEgg = false
+
+    local function collectEggs()
+        for _, v in pairs(workspace.Interactions.Nodes.Eggs.ActiveNodes:GetDescendants()) do
+            if v.Name == "Egg" then
+                local eggPosition = v.Position
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(eggPosition)
+                
+                wait(1)
+                
+                for i = 1, 20 do
+                    local args = { tostring(i) }
+                    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CollectEggRemote"):InvokeServer(unpack(args))
+                end
+                
+                break
+            end
+        end
+    end
+    
+    EggCollectToggle:OnChanged(function()
+        if Options.EggCollect.Value then
+            isCollectingEggs = true
+            while isCollectingEggs do
+                collectEggs()
+                wait(0.5)
+            end
+        else
+            isCollectingEggs = false
+        end
+        print("Toggle changed:", Options.EggCollect.Value)
+    end)
+
+    Options.EggCollect:SetValue(false)
+
+    local ChestCollectToggle = Tabs.Main:AddToggle("ChestToggle", {Title = "AUTO - Collect(CHEST)", Default = false })
+
+    ChestCollectToggle:OnChanged(function()
+        print("Toggle changed:", Options.ChestToggle.Value)
+    end)
+
+    Options.ChestToggle:SetValue(false)
+
+end
