d49c75002e64179d3fb4ffa413248dc1513df727
Vichian <68704647+VichianCreater@users.noreply.github.com>
2025-10-08 22:01:03 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -541,6 +541,31 @@ do
         end
     })
 
+    Tabs.TeleportMap:AddButton({
+        Title = "HOP Server",
+        Description = "Click To Teleport HOP Server",
+        Callback = function()
+            local TeleportService = game:GetService("TeleportService")
+            local HttpService = game:GetService("HttpService")
+
+            local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+            local Server, Next = nil, nil
+            local function ListServers(cursor)
+                local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
+                return HttpService:JSONDecode(Raw)
+            end
+            repeat
+                local Servers = ListServers(Next)
+                Server = Servers.data[math.random(1, (#Servers.data / 3))]
+                Next = Servers.nextPageCursor
+            until Server
+
+            if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+                TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
+            end
+        end
+    })
+
 end
 
 Window:SelectTab(1)
