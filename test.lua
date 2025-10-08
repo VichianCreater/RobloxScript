8042313244d7f4bb88c676b1e124eba57a792ecd
Vichian <68704647+VichianCreater@users.noreply.github.com>
2025-10-08 08:48:31 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Dragon Adventure | Test Script",
+    Title = "Dragon Adventure | Test Script 1.1.0",
     SubTitle = "By Vichian",
     TabWidth = 160,
     Size = UDim2.fromOffset(480, 360),
@@ -69,26 +69,26 @@ do
 
             -- วนลูปผ่านทุกๆ LargeFoodNode ที่เก็บไว้ใน allNodes
             for _, v in ipairs(allNodes) do
-                local targetPosition = v.WorldPivot.Position
                 local BillboardPart = v:FindFirstChild("BillboardPart")
                 
                 if BillboardPart then
                     local Health = BillboardPart:FindFirstChild("Health")
+                    local billboardPos = BillboardPart.WorldPivot.Position
                     if Health then
                         -- ถ้า Health > 0 ให้ตีจนกว่า Health จะเป็น 0
                         while Health.Value > 0 do
                             attackTree(BillboardPart)
-                            wait(1)  -- รอระหว่างการโจมตี
+                            wait(0.2)  -- รอระหว่างการโจมตี
                         end
 
                         if firstProgress then
-                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(targetPosition + Vector3.new(0, 60, 0))
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(billboardPos + Vector3.new(0, 60, 0))
                             firstProgress = false
                         end
 
                         -- หลังจาก Health เป็น 0 แล้ว, วาร์ปไปตำแหน่งถัดไป
                         if Health.Value <= 0 then
-                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(targetPosition + Vector3.new(0, 60, 0))
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(billboardPos + Vector3.new(0, 60, 0))
                             print("Tree destroyed, moving to next target!")
                             break  -- ออกจากลูปเพื่อไปวาร์ปที่อื่น
                         end
