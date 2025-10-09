444e32080a6834b056652be93f8f6bf07caf40ea
Vichian <68704647+VichianCreater@users.noreply.github.com>
2025-10-09 20:58:20 +0700

    Update autocoin.lua

--- autocoin.lua
+++ autocoin.lua
@@ -11,7 +11,7 @@ if game.GameId ~= ALLOWED_GAME_ID then
 end
 
 local Window = Fluent:CreateWindow({
-    Title = "DA / Auto Coin",
+    Title = "DA | Auto Coin",
     SubTitle = "By Vichian",
     TabWidth = 160,
     Size = UDim2.fromOffset(480, 360),
@@ -172,6 +172,57 @@ do
             end
 
             if game.PlaceId == 125804922932357 then
+                local Players = game:GetService("Players")
+
+                local playerList = Players:GetPlayers()
+                local playerCount = #playerList
+
+                print("มีผู้เล่นทั้งหมด " .. playerCount .. " คน")
+                if playerCount <= 5 then
+                    local TeleportService = game:GetService("TeleportService")
+                    local HttpService = game:GetService("HttpService")
+
+                    local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+                    local Server, Next = nil, nil
+                    local function ListServers(cursor)
+                        local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
+                        return HttpService:JSONDecode(Raw)
+                    end
+                    repeat
+                        local Servers = ListServers(Next)
+                        Server = Servers.data[math.random(1, (#Servers.data / 3))]
+                        Next = Servers.nextPageCursor
+                    until Server
+
+                    if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+                        TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
+                    end
+                end
+
+                for _, player in pairs(playerList) do
+                    print(player.Name)
+                    if player.Name == 'Setto2001' or player.Name == 'ZVCK' then
+                        local TeleportService = game:GetService("TeleportService")
+                        local HttpService = game:GetService("HttpService")
+
+                        local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+                        local Server, Next = nil, nil
+                        local function ListServers(cursor)
+                            local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
+                            return HttpService:JSONDecode(Raw)
+                        end
+                        repeat
+                            local Servers = ListServers(Next)
+                            Server = Servers.data[math.random(1, (#Servers.data / 3))]
+                            Next = Servers.nextPageCursor
+                        until Server
+
+                        if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+                            TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
+                        end
+                    end
+                end
+
                 if EdamameCount.Value >= 500 and MistSudachiCount.Value >= 500 and KajiFruitCount.Value >= 500 then
                     print("Full")
                     StartHavest = false
